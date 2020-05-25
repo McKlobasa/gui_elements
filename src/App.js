6fef23130b0ac9254fb0d6f7e044b57c199b6111
McKlobasa <kl.lampret@gmail.com>
2020-05-25 10:27:18 +0200

    new project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styled, { createGlobalStyle } from 'styled-components'
+
+import Button from './components/graphical/Button.js'
+
+const GlobalStyle = createGlobalStyle`
+  :root {
+    --mainColor1: #5F5F5F;
+    --mainColor2: #55BA96;
+    --mainColor3: #DBF0E9;
+    --textColor: #757575;
+  }
+  #root {
+    height: 100%
+  }
+  body {
+    box-sizing: border-box;
+    font-family: sans-serif;
+    height: 100%;
+    width: 100%;
+    margin: 0;
+    position: relative;
+    overflow: scroll;
+    font-size: 1.5em;
+    background: white;
+    padding: 50px;
+  }
+  html {
+    height: 100%;
+    width: 100%;
+    margin: 0;
+  }
+`
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GlobalStyle />
+        <Button>touch me</Button>
     </div>
   );
 }
